46604617f3a3791ac0f4c5b9c6f61f07ccad3dec
Chidozie Zeno Ezeanekwe <chidozieezeanekwe@gmail.com>
2023-04-08 12:19:09 +0100

    add other authentication methods

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-
-const encrypt = require('mongoose-encryption')
 const ejs = require('ejs')
+const session = require('express-session')
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
 
 const app = express()
 
@@ -17,6 +18,16 @@ app.use(
   })
 )
 
+// initialize Session
+app.use(
+  session({ secret: 'keyboard cat', resave: false, saveUninitialized: false })
+)
+
+// initialize passport
+app.use(passport.initialize())
+// initialize passport to use the session package
+app.use(passport.session())
+
 // use mongoose to connect to mongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/userDB', { useNewUrlParser: true })
 
@@ -27,13 +38,19 @@ const userSchema = new mongoose.Schema({
   password: String,
 })
 
-// use the long unguessable string to encrypt the database
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ['password'],
-})
+// hash and salt paswords and save the hash to our database
+userSchema.plugin(passportLocalMongoose)
 
+// sets up mongoose to use the schema
 const User = new mongoose.model('user', userSchema)
+
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy())
+
+// creates cookie
+passport.serializeUser(User.serializeUser())
+// use cookie
+passport.deserializeUser(User.deserializeUser())
 
 // routes
 app.get('/', (req, res) => {
@@ -45,28 +62,39 @@ app.get('/login', (req, res) => {
 app.get('/register', (req, res) => {
   res.render('register')
 })
+app.get('/secrets', (req, res) => {
+  req.isAuthenticated ? res.render('secrets') : res.render('login')
+})
+
+app.get('/logout', (req, res) => {
+  req.logOut((err) => {
+    err ? console.log(err, 'unable to log out') : res.redirect('/')
+  })
+})
 
 //? =========================== REgister Function
+// Sets up a POST route for registering a new user
 app.post('/register', (req, res) => {
-  // sign up new users via the form in the register file
-  const newUser = new User({
-    // "username" here is the name attribute of the input html element for the email
-    email: req.body.username,
-
-    // "password" here is the name attribute of the input html element for the password
-    password: req.body.password,
-  })
-
-  // function to save the new user...
-  newUser
-    .save()
-    // ...and grant them access to the "secrets page"
-    .then((user) => {
-      res.render('secrets')
-    })
-    .catch((err) => {
-      console.log(err, 'error while saving user')
-    })
+  // Calls the User model's register method to create a new user in the database
+  User.register(
+    { username: req.body.username }, // Uses the username from the request body
+    req.body.password, // Uses the password from the request body
+    (err, user) => {
+      // Callback function that will be called after User.register finishes
+      if (err) {
+        // If there was an error, logs the error message to the console
+        console.log(err, 'There was an error in registering user')
+        // Redirects the user back to the registration page
+        res.redirect('/register')
+      } else {
+        // If there was no error, authenticates the user with Passport's 'local' strategy
+        passport.authenticate('local')(req, res, () => {
+          // Redirects the user to the secrets page
+          res.redirect('/secrets')
+        })
+      }
+    }
+  )
 })
 
 //? ===================End of REgister Function
@@ -74,41 +102,17 @@ app.post('/register', (req, res) => {
 //? =========================== Login function
 // login users via the form in the login file
 app.post('/login', async (req, res) => {
-  // "username" here is the name attribute of the input html element for the email
-  const username = req.body.username
-  // "password" here is the name attribute of the input html element for the password
-  const password = req.body.password
-
-  try {
-    // find a user with the inputed email
-    const foundUser = await User.findOne({ email: username })
-
-    // if the user is found, check if the password matches
-    if (foundUser.password === password) {
-      // if the password matches, grant access to the "secrets" page
-      res.render('secrets')
-    }
-  } catch (err) {
-    console.log(
-      err,
-      'there was an issue finding the email that matches the user'
-    )
-  }
-
-  // User.findOne({ email: username }, (err, foundUser) => {
-  //   if (err) {
-  //     console.log(
-  //       err,
-  //       'there was an issue finding the email that matches the user'
-  //     )
-  //   } else {
-  //
-  //     if (foundUser.password === password) {
-  //
-  //       res.render('secrets')
-  //     }
-  //   }
-  // })
+  const user = new User({
+    username: req.body.username, // Uses the username from the request body
+    password: req.body.password, // Uses the password from the request body
+  })
+  req.logIn(user, (err) => {
+    err
+      ? res.redirect('/login')
+      : passport.authenticate('local')(req, res, () => {
+          res.redirect('/secrets')
+        })
+  })
 })
 //? =========================== end of Login function
 
